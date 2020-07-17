20cd634c35e0102ed405129ab9050f62d746bdcb
bzozoo <47896536+bzozoo@users.noreply.github.com>
2020-07-17 22:27:46 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,4 +1,5 @@
 <div style="color: white; background-color: red;"><?php
-echo "Ez az első HEROKU WEB applikációm 😁";
+echo "Ez az első HEROKU web applikációm 😁 Ez PHP-t használ a HTML weboldalak kirajzolására, de lehetőség van NodeJS használatára is. Próbáljátok ki ti is. 🙂";
 ?>
 </div>
+<a href="php.info">PHP szerverinformációk</a>
